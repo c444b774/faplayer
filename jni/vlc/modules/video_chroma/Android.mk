5f6ff8b461f504f65d39eaff6673f94baa44cd4a
tewilove <tewilove@gmail.com>
2011-03-12 14:46:41 +0800

    use asm optimized yuv2rgb from theorarm

---
+++ jni/vlc/modules/video_chroma/Android.mk
@@ -0,0 +1,37 @@
+
+LOCAL_PATH := $(call my-dir)
+
+#libyuv2rgb_plugin.so
+
+include $(CLEAR_VARS)
+
+LOCAL_ARM_MODE := arm
+
+LOCAL_MODULE := yuv2rgb_plugin
+
+LOCAL_CFLAGS += \
+    -std=c99 \
+    -D__THROW= \
+    -DHAVE_CONFIG_H \
+    -DNDEBUG \
+    -D__PLUGIN__ \
+    -DMODULE_STRING=\"yuv2rgb\"
+
+LOCAL_CPPFLAGS += $(COMMON_OPT_CFLAGS)
+LOCAL_LDFLAGS += $(COMMON_OPT_LDFLAGS)
+
+LOCAL_C_INCLUDES += \
+    $(VLCROOT)/compat \
+    $(VLCROOT) \
+    $(VLCROOT)/include \
+    $(VLCROOT)/src \
+    $(EXTROOT)/yuv2rgb
+
+LOCAL_SRC_FILES := \
+    yuv2rgb.c
+
+LOCAL_STATIC_LIBRARIES += yuv2rgb
+LOCAL_SHARED_LIBRARIES += vlccore
+
+include $(BUILD_SHARED_LIBRARY)
+
