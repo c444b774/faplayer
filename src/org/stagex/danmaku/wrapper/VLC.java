8ca9b78237f97670be39ba24ec2dccb51cce80db
tewilove <tewilove@gmail.com>
2011-03-20 19:47:56 +0800

    many bug fixes

---
+++ src/org/stagex/danmaku/wrapper/VLC.java
@@ -0,0 +1,49 @@
+package org.stagex.danmaku.wrapper;
+
+import android.view.Surface;
+
+public class VLC {
+
+	static {
+		System.loadLibrary("vlccore");
+	}
+
+	private static VLC mInstance = null;
+
+	private int mHandle = 0;
+
+	private Thread mVLCMain;
+
+	protected VLC() {
+	}
+	
+	public static native int setenv(String key, String val, boolean overwrite);
+
+	private native void run(String[] args);
+
+	public static native void attachVideoOutput(Surface surface);
+
+	public static native void detachVideoOutput();
+
+	public static VLC getInstance() {
+		if (mInstance == null)
+			mInstance = new VLC();
+		return mInstance;
+	}
+
+	public void create(final String[] args) {
+		if (mVLCMain != null && mVLCMain.isAlive())
+			return;
+		mVLCMain = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				VLC.this.run(args);
+			}
+		});
+		mVLCMain.start();
+	}
+
+	public void destroy() {
+		// ...
+	}
+}
